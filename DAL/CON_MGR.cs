4613f6a884d3d30a273e596f05c099a74bfd9590
IrwinnSama <warnier.fabian@gmail.com>
2022-04-13 16:09:54 +0200

    Creation connection SQL Server

---
+++ DAL/CON_MGR.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DAL
+{
+    public static class CON_MGR
+    {
+        public static IDbConnection Connection()
+        {
+            return new SqlConnection(@"Data Source=shady.ddns.net;Initial Catalog=DB_SGB_PROJECT_V2-1;Persist Security Info=True;User ID=sa;Password=this=Password**");
+        }
+        
+    }
+}
