c28f2ed1c62f22f2b05cd0353e79a81167cc2a00
IrwinnSama <warnier.fabian@gmail.com>
2022-04-14 16:42:33 +0200

    color

---
+++ PL/Assets/CustomColor.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL
+{
+    public static class CustomColor
+    {
+        public static Color Custom1 
+        {
+            get        
+            {                
+                return Color.FromArgb(7, 8, 38);
+            }
+        }
+
+        public static Color Custom2
+        {
+            get
+            {
+                return Color.FromArgb(134, 84, 57);
+            }
+        }
+
+        public static Color Custom3
+        {
+            get
+            {
+                return Color.FromArgb(198, 139, 89);
+            }
+        }
+
+        public static Color Custom4
+        {
+            get
+            {
+                return Color.FromArgb(215, 177, 157);
+            }
+        }
+    }
+}
