c67928fd4aae1c726078442387ac1facec4d4895
shadysx <laurent.kl1996@gmail.com>
2022-04-14 17:42:57 +0200

    button

--- PL/Login.Designer.cs
+++ PL/Login.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.textBoxUserName = new CustomControls.RJControls.RJTextBox();
             this.rjTextBox1 = new CustomControls.RJControls.RJTextBox();
+            this.rjButton1 = new CustomControls.RJControls.RJButton();
             this.SuspendLayout();
             // 
             // textBoxUserName
@@ -76,12 +77,31 @@
             this.rjTextBox1.Texts = "Password";
             this.rjTextBox1.UnderlinedStyle = false;
             // 
+            // rjButton1
+            // 
+            this.rjButton1.BackColor = System.Drawing.Color.MediumSlateBlue;
+            this.rjButton1.BackgroundColor = System.Drawing.Color.MediumSlateBlue;
+            this.rjButton1.BorderColor = System.Drawing.Color.PaleVioletRed;
+            this.rjButton1.BorderRadius = 0;
+            this.rjButton1.BorderSize = 0;
+            this.rjButton1.FlatAppearance.BorderSize = 0;
+            this.rjButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.rjButton1.ForeColor = System.Drawing.Color.White;
+            this.rjButton1.Location = new System.Drawing.Point(474, 300);
+            this.rjButton1.Name = "rjButton1";
+            this.rjButton1.Size = new System.Drawing.Size(150, 40);
+            this.rjButton1.TabIndex = 2;
+            this.rjButton1.Text = "rjButton1";
+            this.rjButton1.TextColor = System.Drawing.Color.White;
+            this.rjButton1.UseVisualStyleBackColor = false;
+            // 
             // Login
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDark;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.rjButton1);
             this.Controls.Add(this.rjTextBox1);
             this.Controls.Add(this.textBoxUserName);
             this.Name = "Login";
@@ -94,5 +114,6 @@
 
         private CustomControls.RJControls.RJTextBox textBoxUserName;
         private CustomControls.RJControls.RJTextBox rjTextBox1;
+        private CustomControls.RJControls.RJButton rjButton1;
     }
 }
