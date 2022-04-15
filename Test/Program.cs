4df48b21697f5f721941f2a722db481bf59aa160
shadysx <laurent.kl1996@gmail.com>
2022-04-15 19:41:23 +0200

    init auth

---
+++ Test/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BLL;
+using DAL;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Auth.Login("ShadySX", "password");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.WriteLine(Auth.CurrentUser);
+
+
+
+            Console.Read();
+        }
+    }
+}
