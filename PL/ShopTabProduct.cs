42cc67298564f67e5a998463be1f9bda92db9108
IrwinnSama <warnier.fabian@gmail.com>
2022-06-04 23:04:21 +0200

    On merge

--- PL/ShopTabProduct.cs
+++ PL/ShopTabProduct.cs
@@ -12,7 +12,8 @@ namespace PL
 {
     public partial class ShopTabProduct : Form
     {
-        public ShopTabProduct(Form parent, string productName, string productType, string productImagePath, decimal bestPrice)
+        private string description;
+        public ShopTabProduct(string productName, string productType, string description, string productImagePath, decimal bestPrice)
         {
             InitializeComponent();
             this.labelNameArticle.Text = productName;
@@ -20,6 +21,7 @@ namespace PL
             this.labelTypeArticle.Text = productType;
             this.pictureBox1.ImageLocation = productImagePath;
             this.iconButton1.IconColor = CustomColor.Orange;
+            this.description = description;
         }
 
         private void Article_Load(object sender, EventArgs e)
@@ -28,7 +30,7 @@ namespace PL
 
         private void iconButton1_Click(object sender, EventArgs e)
         {
-            Main.mainInstance.OpenChildForm(new DetailsTab(this.labelNameArticle.Text, this.pictureBox1.Image));
+            Main.mainInstance.OpenChildForm(new DetailsTab(this.labelNameArticle.Text, this.description, this.pictureBox1.Image));
         }
     }
 }
